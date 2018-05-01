71f9b3c7fb8edbdacfdf19034d726bf92f4890da
goncalopalaio <goncalopalaio@gmail.com>
2018-05-01 14:57:09 +0100

    adds filter by distance

--- reporting/views.py
+++ reporting/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from rest_framework import viewsets
-from rest_framework import generics
+from rest_framework import viewsets, generics
+from django.contrib.gis.measure import D
+from django.contrib.gis.geos import fromstr
 from .models import Event, Category
 from .serializers import EventSerializer, CategorySerializer
 
@@ -24,7 +25,24 @@ class EventViewSet(viewsets.ModelViewSet):
 		if category_id is not None:
 			queryset = queryset.filter(category_id = category_id)
 
+		
+		queryset = self.filter_by_distance(self.request.query_params, queryset)
 		return queryset
+	
+	def filter_by_distance(self, query_params, queryset):
+		# ignore missing parameters
+		lat = self.request.query_params.get('lat', None)
+		lon = self.request.query_params.get('lon', None)
+		dist_km = self.request.query_params.get('dist_km', None)
+
+		if lat is not None and lon is not None and dist_km is not None:
+			# @note these fields should be validated further to ensure the point object can always be built and the distance is correct
+			point = fromstr("POINT({} {})".format(lat, lon))
+			if point.valid:
+				queryset = queryset.filter(location__distance_lte = (point, D(km = dist_km)))
+
+		return queryset
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
 	"""
