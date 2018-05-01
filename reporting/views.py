63b66658e9e02c7f893441cd181bd443ee3f5c1f
goncalopalaio <goncalopalaio@gmail.com>
2018-05-01 13:58:37 +0100

    filtering based of category_id

--- reporting/views.py
+++ reporting/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from rest_framework import viewsets
+from rest_framework import generics
 from .models import Event, Category
 from .serializers import EventSerializer, CategorySerializer
 
@@ -10,8 +11,20 @@ class EventViewSet(viewsets.ModelViewSet):
 	"""
 	Endpoint that allows events to be viewed or edited
 	"""
-	queryset = Event.objects.all()
 	serializer_class = EventSerializer
+	queryset = Event.objects.all()
+
+	def get_queryset(self):
+		"""
+		Overrides default behaviour to support filtering against query parameters
+		"""
+		queryset = Event.objects.all()
+		
+		category_id = self.request.query_params.get('category_id', None)
+		if category_id is not None:
+			queryset = queryset.filter(category_id = category_id)
+
+		return queryset
 
 class CategoryViewSet(viewsets.ModelViewSet):
 	"""
@@ -19,3 +32,4 @@ class CategoryViewSet(viewsets.ModelViewSet):
 	"""
 	queryset = Category.objects.all()
 	serializer_class = CategorySerializer
+
