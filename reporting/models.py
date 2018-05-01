792d7235af9bfda0129b4536855c43a1feb0c7e6
goncalopalaio <goncalopalaio@gmail.com>
2018-05-01 04:13:19 +0100

    adds creation and edit date

--- reporting/models.py
+++ reporting/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 class Category(models.Model):
 	"""
@@ -6,10 +8,17 @@ class Category(models.Model):
 	"""
 	title = models.CharField(max_length=30, unique = True)
 	description = models.CharField(max_length=200, default = '')
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
 		
 class Event(models.Model):
 	"""
 	Describes an event
 	"""
+	def get_default_category():
+		return Category.objects.get_or_create(title = "default", created=datetime.datetime.now(tz=timezone.utc))[0].id
+
 	description = models.CharField(max_length = 200)
-	category = models.ForeignKey(Category, on_delete=models.CASCADE, default = lambda: Category.objects.get_or_create(title = "default")[0])
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, default = get_default_category)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
--- reporting/serializers.py
+++ reporting/serializers.py
@@ -15,4 +15,4 @@ class EventSerializer(serializers.ModelSerializer):
 	"""
 	class Meta:
 		model = Event
-		fields= ('url','description', 'category')
+		fields= ('url','description', 'category', 'created', 'updated')
